f56c20f041db70cdbde52a97837711dcd1fe0e6d
AKSHAYA SHREE B A <111514393+AkshayaShree28@users.noreply.github.com>
2023-03-07 10:52:29 +0530

    Create Median.py

---
+++ Median.py
@@ -0,0 +1,41 @@
+Find the Median for given array
+
+
+
+
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'findMedian' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def findMedian(arr):
+    # Write your code here
+    arr.sort()
+    l=len(arr)
+    Median=l//2
+    res=(arr[Median]+arr[~Median])/2
+    result=int(res)
+    return result
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = findMedian(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
